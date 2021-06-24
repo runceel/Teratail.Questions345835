bad62ccbee0de311f2c372f668ccb99ef95c4db7
Kazuki Ota <kaota@microsoft.com>
2021-06-24 19:54:06 +0900

    Add project files.

---
+++ Teratail.Questions345835/NowData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Teratail.Questions345835
+{
+    class NowData
+    {
+        public static Dictionary<string, SolidColorBrush> Colors { get; } =
+            new Dictionary<string, SolidColorBrush>
+            {
+                {"Red", Brushes.Red },
+                {"Blue", Brushes.Blue },
+                {"Green", Brushes.Green },
+                {"白", Brushes.White },
+                {"黒", Brushes.Black },
+                {"透明", Brushes.Transparent },
+            };
+    }
+}
